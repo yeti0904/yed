af4df4ae0aa808114f2ff02330feb23a853748b7
yeti0904 <mesyeti@protonmail.com>
2022-10-01 18:25:30 +0100

    updated last used file name when opening a file using command line parameters

--- src/main.cc
+++ src/main.cc
@@ -52,6 +52,7 @@ int main(int argc, char** argv) {
             
                 key += lineDistance;
             }
+            lastFileName = argv[i];
             printf("opened %s\n", argv[i]);
         }
     }
